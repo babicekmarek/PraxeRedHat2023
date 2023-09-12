11024d31ed2a49c96bac6b7bd6ac4b4bfc485fa5
babicekmarek <babicek.marek@gmail.com>
2023-09-12 14:23:01 +0200

    osmnact

--- obsahTrojuhelniku.py
+++ obsahTrojuhelniku.py
@@ -1,5 +1,7 @@
 import math
 def areaTriangle(a,b,c):
-    obsah=math.sqrt(((a+b+c)/2)*(((a+b+c)/2)-a)*(((a+b+c)/2)-b)*(((a+b+c)/2)-c))    
-    return obsah
+    if a+b>c and b+c> and a+c>b:
+        return math.sqrt(((a+b+c)/2)*(((a+b+c)/2)-a)*(((a+b+c)/2)-b)*(((a+b+c)/2)-c)) 
+    else:
+        return None
 
