681570a4371d1b6687a6fefc330388be78e4e92a
babicekmarek <babicek.marek@gmail.com>
2023-09-12 14:26:50 +0200

    dvacet

--- obsahTrojuhelniku.py
+++ obsahTrojuhelniku.py
@@ -1,6 +1,6 @@
 import math
 def areaTriangle(a,b,c):
-    if a+b>c and b+c> and a+c>b:
+    if a+b>c and b+c>a and a+c>b:
         return math.sqrt(((a+b+c)/2)*(((a+b+c)/2)-a)*(((a+b+c)/2)-b)*(((a+b+c)/2)-c)) 
     else:
         return None
